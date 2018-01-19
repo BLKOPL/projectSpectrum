4e14fd149870dcb288d83eafb3a655e9e481a00d
Beza <bezawit.teferi@gmail.com>
2018-01-18 21:33:33 -0500

    api calls fixed- next stage Change to the new version for section 3 and 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,10 @@ var LinktoArticle
 
 function reachNYTapi(){
 
-// if (userInput){
+userInput=$("#searchBar").val().trim();
+if (userInput){
  // If search item is Nan this will not let the user click submit
-console.log(userInput);
+
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9bbd8167715c47cf98ffd217a669c995&sort=newest&q=" + userInput ;
 
     $.ajax({
@@ -14,45 +15,52 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key
       method: "GET"
     })
     .done(function(NYTresponse) {
-           console.log(NYTresponse.response.docs);
+           // console.log(NYTresponse.response.docs);
 
 //////////////////////////////////////////////////////////////
 
 
 // my var called NytData holds the result/////////////////////
-     // var NytData = NYTresponse.response.docs;
+     var NytData = NYTresponse.response.docs;
+     console.log(NytData);
      var Articletitle =NYTresponse.response.docs[0].headline.main;
-     // console.log(Articletitle);
+     console.log(Articletitle);
      var LinktoArticle=NYTresponse.response.docs[0].web_url
+     var publishedAt  =NYTresponse.response.docs[0].pub_date
+     console.log(publishedAt);
+     var title1 = NYTresponse.response.docs[0].headline.main
+
 
      console.log(LinktoArticle);
      var ArticleImage ="http://www.nytimes.com/"+NYTresponse.response.docs[0].multimedia[0].url;
      console.log(ArticleImage);
     var thumbnail_image =$("<img>");
       thumbnail_image.attr('src',ArticleImage);
-  $("#display-blue-article").html(thumbnail_image)
+  $("#display-blue-article").html(thumbnail_image )
+  $(".titleBlue").html(title1);
+    $(".time_date").html(publishedAt);
 
     });
 }
-// }
+}
 ////////////////////////////////////////////////////////////
-
-
-
-
-
-//API call for WSJ////////////////////////////////////
+//
+//
+//
+//
+//
+// //API call for WSJ////////////////////////////////////
 
 
 
 function reachWSJapi(){
- // userInput = $("#searchBar").val().trim();
+ userInput = $("#searchBar").val().trim();
+ if (userInput){
 
 var queryURLWSJ = 'https://newsapi.org/v2/everything?' +
          'q=' + userInput + '&' +
          'sources=' + 'the-wall-street-journal' + '&' +
-         'from=2018-01-15&' + //change date with moment
-         'sortBy=popularity&' +
+         'sortBy=publishedAt&' +
          'apiKey=29a6e2a1dcf34cc9a4528d592d8b5ed8';
 
 
@@ -61,7 +69,10 @@ var queryURLWSJ = 'https://newsapi.org/v2/everything?' +
        method: "GET"
      }).done(function(response) {
        console.log(response);
-
+          if (response.articles.length==0){
+            alert(" display no article on the html for this section")
+          }
+          else{
        for (i=0; i<response.articles.length; i++) {
          var source = response.articles[i].source.id
          var title = response.articles[i].title
@@ -72,29 +83,34 @@ var queryURLWSJ = 'https://newsapi.org/v2/everything?' +
          }
        };
   var imageWSJ = response.articles[0].urlToImage;
+  var time_date2  = response.articles[0].publishedAt
+  var title2 = response.articles[0].title
+  var description=response.articles[0].title.description
+  console.log(imageWSJ);
   var thumbnail_image =$("<img>");
       thumbnail_image.attr('src', imageWSJ);
   $("#display-neutral-article").html(thumbnail_image)
+  $(".titleNeutral").html(title2);
+  $(".time_date2").html(description);
 
-
+}
      });
 
 }
-///////////////////////////////////////////////////////
-
-
-
-
-
-//API call for BB/////////////////////////////////////
+}
+// // ///////////////////////////////////////////////////////
+//
+// //
+// //
+// //
+// // //API call for BB/////////////////////////////////////
 function reachBBapi(){
-// userInput=$("#searchBar").val().trim();
+userInput=$("#searchBar").val().trim();
 
 var queryURLBreitbart = 'https://newsapi.org/v2/everything?' +
          'q=' + userInput + '&' +
          'sources=' + 'breitbart-news' + '&' +
-         'from=2018-01-15&' + //change date with moment
-         'sortBy=popularity&' +
+         'sortBy=publishedAt&' +
          'apiKey=29a6e2a1dcf34cc9a4528d592d8b5ed8';
 
 
@@ -102,42 +118,65 @@ var queryURLBreitbart = 'https://newsapi.org/v2/everything?' +
        url: queryURLBreitbart,
        method: "GET"
      }).done(function(response) {
-       console.log(response);
+
+       console.log(response.articles);
+       if (response.articles.length==0){
+         alert(" display no article on the html for this section")
+       }
+         else{
 
        for (i=0; i<response.articles.length; i++) {
          var source = response.articles[i].source.id
          var title = response.articles[i].title
          console.log(source);
          if (source === "breitbart-news") {
-           console.log(title);
+           // console.log(title);
            break;
          }
        };
 
+// code error on 119
    var imageBB = response.articles[0].urlToImage;
-  var thumbnail_image =$("<img>");
-      thumbnail_image.attr('src', imageBB);
-  $("#display-neutral-article").html(thumbnail_image)
+   var time_date3  = response.articles[0].publishedAt
+   var title3 = response.articles[0].title
+   console.log(imageBB);
+   var thumbnail_image =$("<img>");
+       thumbnail_image.attr('src', imageBB);
+   $("#display-neutral-article").html(thumbnail_image)
+   $(".titleRed").html(title3);
+   $(".time_date3").html(time_date3);
 
+}
 
      });
    }
 //////////////////////////////////////////////////////
 
-
+// create a var that holds a button
+// then when user hits submit call this button
+// when user clicks on this button call the #submitBtn function
 
 //when user clicks submit////////////////////////////
 $("#submitBtn").on("click", function(event){
 event.preventDefault();
 
-userInput=$("#searchBar").val().trim();
-if (userInput){
 reachNYTapi();
 reachWSJapi();
 reachBBapi();
-}
+
 
 });
+
+$("#submitBtn").keyup(function(event){
+    if(event==13){
+      event.preventDefault();
+
+      reachNYTapi();
+      reachWSJapi();
+      reachBBapi();
+    } // missing closing if brace
+});
+
 
 $("#nytArticle").append(LinktoArticle);
 $("#wsjArticle").append(LinktoArticle);
