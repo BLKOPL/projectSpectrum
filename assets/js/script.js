f3c21b6dd5b3e3a84b72f1ac94056abb320a2e2f
Li Chien <lichien@Lis-MacBook-Pro.local>
2018-01-23 21:20:11 -0500

    li rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,126 +1,348 @@
 //API call for NYT////////////////////////////////////////////
-var Search_item
+var userInput
 var LinktoArticle
+var articleCounter = 0;
+var NytHeader
+var WsjHeader
+var BbHeader
+var nYT_articleImage
+var nyt_snipit
+var NytData // nyt Respo
+var wSjData // WSJ repo holder
+var WSJ_articleImage
+var section4_ArticleLocation ="leftSide" // current div the image is clicked on left side or right side
+// section4_ArticleLocation will be used below in an if else
 
-function reachNYTapi(){
 
-Search_item=$("#searchBar").val().trim();
-if (Search_item){
- // If search item is Nan this will not let the user click submit
-console.log(Search_item);
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9bbd8167715c47cf98ffd217a669c995&sort=newest&q=" + Search_item ;
+// the below var is to make the next click on the title to be appended to the left/right section based on what was clicked first
+var currentSelectedArticle=""
+
+
+function reachNYTapi() {
+
+  userInput = $("#searchBar").val().trim();
+  if (userInput) {
+    // If search item is Nan this will not let the user click submit
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9bbd8167715c47cf98ffd217a669c995&sort=newest&q=" + userInput;
 
     $.ajax({
-      url: queryURL,
+        url: queryURL,
+        method: "GET"
+      })
+      .done(function(NYTresponse) {
+        // console.log(NYTresponse.response.docs);
+        NytData = NYTresponse.response.docs;
+        // console.log(NytData);
+
+        // forloop that will display the 5 headers.
+        $("#display-blue-article").empty();
+
+        for (i = 0; i < 5; i++) {
+
+          // article link for read more
+          // console.log(nytArticleLink);
+          var nytArticleLink = NYTresponse.response.docs[i].web_url;
+          // console.log(nytArticleTitles);
+          var nytArticleTitles = NYTresponse.response.docs[i].headline.main;
+
+          // date and time //
+          var publishedAt = NYTresponse.response.docs[i].pub_date
+
+          // console.log(nYT_articleImage);
+
+          articleCounter++;
+
+          // maybe add a class on each list item and use on click function later on and say this.
+
+          NytHeader = "<a onClick='displayNYTsection4(" + i + ")' href='#' ><li class='Nyt_article_list list-group-item'>" + nytArticleTitles + "</li></a>";
+
+          $("#display-blue-article").append(NytHeader);
+
+        };
+
+      });
+  }
+}
+
+// //API call for WSJ////////////////////////////////////
+
+function reachWSJapi() {
+  userInput = $("#searchBar").val().trim();
+  if (userInput) {
+
+    var queryURLWSJ = 'https://newsapi.org/v2/everything?' +
+      'q=' + userInput + '&' +
+      'sources=' + 'the-wall-street-journal' + '&' +
+      'sortBy=publishedAt&' +
+      'language=en&' +
+      'apiKey=29a6e2a1dcf34cc9a4528d592d8b5ed8';
+
+
+    $.ajax({
+      url: queryURLWSJ,
       method: "GET"
+    }).done(function(WSJresponse) {
+      wSjData = WSJresponse.articles;
+      // console.log(wSjData);
+      // console.log(WSJresponse);
+      // console.log(articles)
+      if (WSJresponse.articles.length == 0) {
+        alert(" display no article on the html for this section")
+      } else {
+
+        //to dynamically create article list
+        $("#display-neutral-article").empty();
+
+        for (i = 0; i < 5; i++) {
+          var wsjArticleTitles = WSJresponse.articles[i].title;
+          // console.log(wsjArticleTitles);
+
+          var wsjArticleLink = WSJresponse.articles[i].url;
+
+          articleCounter++;
+          // maybe add a class on each list item and use on click function later on and say this.
+          WsjHeader = "<a onClick='displayWSJsection4(" + i + ")' href='#' ><li class='Wsj_article_list list-group-item'>" + wsjArticleTitles + "</li></a>";
+
+
+
+
+
+          $("#display-neutral-article").append(WsjHeader);
+
+        }
+      }
+
     })
-    .done(function(NYTresponse) {
-           console.log(NYTresponse.response.docs);
-
-//////////////////////////////////////////////////////////////
-
-
-// my var called NytData holds the result/////////////////////
-     // var NytData = NYTresponse.response.docs;
-     var Articletitle =NYTresponse.response.docs[0].headline.main;
-     // console.log(Articletitle);
-     var LinktoArticle=NYTresponse.response.docs[0].web_url
-
-     console.log(LinktoArticle);
-     var ArticleImage ="http://www.nytimes.com/"+NYTresponse.response.docs[0].multimedia[0].url;
-     console.log(ArticleImage);
-    var thumbnail_image =$("<img>");
-      thumbnail_image.attr('src',ArticleImage);
-  $("#display-blue-article").html(thumbnail_image)
-
-    });
+  }
 }
+// // // ///////////////////////////////////////////////////////
+
+// // // //API call for BB/////////////////////////////////////
+function reachBBapi() {
+  userInput = $("#searchBar").val().trim();
+
+  var queryURLBreitbart = 'https://newsapi.org/v2/everything?' +
+    'q=' + userInput + '&' +
+    'sources=' + 'breitbart-news' + '&' +
+    'sortBy=publishedAt&' +
+    'language=en&' +
+    'apiKey=29a6e2a1dcf34cc9a4528d592d8b5ed8';
+
+
+  $.ajax({
+    url: queryURLBreitbart,
+    method: "GET"
+  }).done(function(BBresponse) {
+
+
+
+    //to dynamically create article list
+    $("#display-red-article").empty();
+
+    for (i = 0; i < 5; i++) {
+      var bbArticleTitles = BBresponse.articles[i].title;
+      // console.log(bbArticleTitles);
+
+      var bbArticleLink = BBresponse.articles[i].url;
+
+      // var ArticleImage ="http://www.nytimes.com/"+NYTresponse.response.docs[i].multimedia.url;
+
+      articleCounter++;
+      // maybe add a class on each list item and use on click function later on and say this.
+
+      BbHeader = "<a onClick='displayBBsection4(" + i + ")' href='#' ><li class='Bb_article_list list-group-item'>" + bbArticleTitles + "</li></a>";
+
+
+
+
+      $("#display-red-article").append(BbHeader);
+
+
+    }
+  })
+
 }
-////////////////////////////////////////////////////////////
+
+// //////////////////////////////////////////////////////
 
 
+//when user clicks submit////////////////////////////
+$("#submitBtn").on("click", function(event) {
+  event.preventDefault();
+
+  reachNYTapi();
+  reachWSJapi();
+  reachBBapi();
+
+});
 
 
-
-//API call for WSJ////////////////////////////////////
-
-var userInput = $("#searchBar").val().trim();
-
-function reachWSJapi(){
+$("#nytArticle").append(LinktoArticle);
+$("#wsjArticle").append(LinktoArticle);
+$("#breitbartArticle").append(LinktoArticle);
 
 
-var queryURLWSJ = 'https://newsapi.org/v2/everything?' +
-         'q=' + userInput + '&' +
-         'sources=' + 'the-wall-street-journal' + '&' +
-         'sortBy=popularity&' +
-         'apiKey=29a6e2a1dcf34cc9a4528d592d8b5ed8';
+// on click event to trigger section 4
+
+// when user clicks link show the link to section 4
 
 
-     $.ajax({
-       url: queryURLWSJ,
-       method: "GET"
-     }).done(function(response) {
-       console.log(response);
+// when user clicks this titleRed in section 3 - the related info for thay section will be poplated in section 4
 
-       for (i=0; i<response.articles.length; i++) {
-         var source = response.articles[i].source.id
-         var title = response.articles[i].title
-         console.log(source);
-         if (source === "the-wall-street-journal") {
-           console.log(title);
-           break;
-         }
-       };
-  var imageWSJ = response.articles[0].urlToImage;
-  var thumbnail_image =$("<img>");
-      thumbnail_image.attr('src', imageWSJ);
-  $("#display-neutral-article").html(thumbnail_image)
+//===========================********=======================Code to make section 4 interavtivr =====******************==================//
+function displayNYTsection4(i) {
 
 
-     });
+  // console.log(NYTresponse.response.docs);
+  // my var called NytData holds the result/////////////////////
+  // updating
+  var nYT_articleImage = $("<img>");
+  nYT_articleImage.attr('src', "http://www.nytimes.com/" + NytData[i].multimedia[i].url);
+
+// where to place the articles in the after the user clicks on one after the other
+
+  if((currentSelectedArticle=="nyt" && section4_ArticleLocation=="rightSide")|| section4_ArticleLocation=="leftSide"){
+
+  $("#left_selected_article").html(NytData[i].snippet)
+  $("#left_selected_image").html(nYT_articleImage);
+  $("#left_selected_date").html(NytData[i].pub_date);
+  section4_ArticleLocation="rightSide";
+  // console.log("http://www.nytimes.com/" + NytData[i].multimedia[i].url);
+  // console.log(NytData[i].pub_date);
+  // console.log(NytData[i])
+  // console.log(NytData[i].snippet)
 }
-///////////////////////////////////////////////////////
+// if the first condition is true then no need to run the second code
+else if((currentSelectedArticle=="nyt" && section4_ArticleLocation=="leftSide")|| section4_ArticleLocation=="rightSide") {
+
+
+    section4_ArticleLocation="leftSide";  // resetting it
+    $("#right_selected_article").html(NytData[i].snippet)
+    $("#right_selected_image").html(nYT_articleImage);
+    $("#right_selected_date").html(NytData[i].pub_date);
+
+}
+currentSelectedArticle="nyt"  // resetting the value of currentSelectedArticle
+
+};
+// =======================================for WSJ===============================================
+function displayWSJsection4(i) {
+  console.log("WSJ HAS BEEN CLICKED - where to display div");
+
+  // wSjData = WSJresponse.articles;
+  // console.log(wSjData);
+  // // console.log(WSJresponse.articles[i].urlToImage)
+  // console.log(wSjData[i].urlToImage)
+  //   console.log(WSJresponse.articles[i].description)
+  //     console.log(WSJresponse.articles[i].publishedAt)
+
+var WSJ_articleImage = $("<img>");
+WSJ_articleImage.attr('src', wSjData[i].urlToImage);
+
+if((currentSelectedArticle=="WSJ" && section4_ArticleLocation=="rightSide")|| section4_ArticleLocation=="leftSide"){
+
+  section4_ArticleLocation="leftSide";  // resetting it
+  $("#right_selected_article").html(wSjData[i].description)
+  $("#right_selected_image").html(WSJ_articleImage);
+  $("#right_selected_date").html(wSjData[i].publishedAt);
+
+}
+currentSelectedArticle="WSJ"
+
+};
+
+//=================================For BB===============================================
+
+function displayBBsection4(i) {
+  console.log("BB HAS BEEN CLICKED - where do we display this");
+
+};
 
 
 
 
 
-//API call for BB/////////////////////////////////////
-function reachBBapi(){
-
-var queryURLBreitbart = 'https://newsapi.org/v2/everything?' +
-         'q=' + userInput + '&' +
-         'sources=' + 'breitbart-news' + '&' +
-         'from=2018-01-15&' + //change date with moment
-         'sortBy=popularity&' +
-         'apiKey=29a6e2a1dcf34cc9a4528d592d8b5ed8';
 
 
-     $.ajax({
-       url: queryURLBreitbart,
-       method: "GET"
-     }).done(function(response) {
-       console.log(response);
-
-       for (i=0; i<response.articles.length; i++) {
-         var source = response.articles[i].source.id
-         var title = response.articles[i].title
-         console.log(source);
-         if (source === "breitbart-news") {
-           console.log(title);
-           break;
-         }
-       };
-
-   var imageBB = response.articles[0].urlToImage;
-  var thumbnail_image =$("<img>");
-      thumbnail_image.attr('src', imageBB);
-  $("#display-neutral-article").html(thumbnail_image)
 
 
-     });
-   }
-//////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+///////////FIREBASE DATA/////////////
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAtpJYe9s2DVVwRHjrR6yrjL6-n9V2j_6k",
+    authDomain: "train-schedule-gwhw7.firebaseapp.com",
+    databaseURL: "https://train-schedule-gwhw7.firebaseio.com",
+    projectId: "train-schedule-gwhw7",
+    storageBucket: "train-schedule-gwhw7.appspot.com",
+    messagingSenderId: "164648736215"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+var ref = database.ref('keywords')
+console.log(ref)
+
+
+ref.on('value', gotData, errData)
+
+function gotData(keyword) {
+  // console.log(data.val())
+var showData = keyword.val()
+console.log(showData)
+$('#thing').append(showData);
+}
+
+function errData(err) {
+  console.log('theres an error boiii')
+
+}
+
+
+
+// var ref = database.ref('searched')
+// create function that puts keywords into firebase
+
+
+function writeUserData(keyword) {
+  firebase.database().ref('keywords').set({
+    keywords: keyword
+  });
+}
+
+// function writeUserData() {
+//   var data = {
+//     searched: keywords
+//   }
+//   console.log(data)
+//   var ref = database.ref('searched')
+//   ref.push(data)
+
+// }
+
+
+
+////getting data//////
+
+
+
+// console.log(ref.path.o)
+
+
+
+
+
+
+
 
 
 
@@ -132,9 +354,38 @@ reachNYTapi();
 reachWSJapi();
 reachBBapi();
 
+//call the function that puts keywords into firebase
+writeUserData(userInput)
+  console.log(writeUserData(userInput));
+  console.log(userInput);
+
+
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $("#nytArticle").append(LinktoArticle);
 $("#wsjArticle").append(LinktoArticle);
 $("#breitbartArticle").append(LinktoArticle);
+
+
+
+
+
+
+
+
